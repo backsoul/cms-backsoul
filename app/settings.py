ad4a0b72c791c999154ea75e1ff5b38480f8685a
BackSoul <danielsarmientodeveloper@gmail.com>
2021-08-27 21:45:39 -0500

    changes and commit

--- app/settings.py
+++ app/settings.py
@@ -24,7 +24,6 @@ SECRET_KEY = 'eqda*kl$7@vdr-$_@u$aj5n9^rp&vbmbu*-3jly%zpc23u0=_^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
 ALLOWED_HOSTS = []
 
 
@@ -115,7 +114,7 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
