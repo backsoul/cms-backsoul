4ed0744a01298e96e8cddebb2faceb99b7fb2e10
BackSoul <danielsarmientodeveloper@gmail.com>
2021-08-27 21:10:39 -0500

    changes and commit

--- app/settings.py
+++ app/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
-        'HOST': 'db',
+        'HOST': 'postgresdb',
         'PORT': '5432',
     }
 }
